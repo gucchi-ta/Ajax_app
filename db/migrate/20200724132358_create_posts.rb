55de99ba6abbf87241afccb608477ce1de980127
gucchi-ta <ta.yamaguchi.1225@gmail.com>
2020-08-17 23:35:22 +0900

    (コメントの編集)

--- db/migrate/20200724132358_create_posts.rb
+++ db/migrate/20200724132358_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
       t.text :content
-      t.boolean :checked    # 既読機能実装時に「既読か未読か」をboolean型で管理
+      t.boolean :checked    # boolean型は「trueまたはfalseの真理値を判断する」型。既読機能実装時に「既読か未読か」をboolean型で管理。
       t.timestamps
     end
   end
